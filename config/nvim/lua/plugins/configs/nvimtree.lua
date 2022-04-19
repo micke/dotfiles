1f9e99c322189f18ae69e2624d4fdbd7faaa1a40
Micke Lisinge <hi@micke.me>
2022-04-19 15:09:13 +0200

    Deprecations

--- config/nvim/lua/plugins/configs/nvimtree.lua
+++ config/nvim/lua/plugins/configs/nvimtree.lua
@@ -11,7 +11,6 @@ vim.o.termguicolors = true
 g.nvim_tree_add_trailing = 0
 g.nvim_tree_git_hl = 1
 g.nvim_tree_highlight_opened_files = 0
-g.nvim_tree_indent_markers = 1
 g.nvim_tree_root_folder_modifier = table.concat { ":t:gs?$?/..", string.rep(" ", 1000), "?:gs?^??" }
 --
 g.nvim_tree_show_icons = {
@@ -76,6 +75,11 @@ nvimtree.setup {
     allow_resize = true,
     side = "left",
     width = 40,
+  },
+  renderer = {
+    indent_markers = {
+      enable = true,
+    },
   },
   git = {
     ignore = false
