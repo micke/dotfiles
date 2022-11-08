a520f4aa98ad0d024041b06864fe332f2dff2c0f
Micke Lisinge <hi@micke.me>
2022-11-08 18:39:59 +0100

    Show matchup matches as floating

--- config/nvim/lua/vim-matchup.lua
+++ config/nvim/lua/vim-matchup.lua
@@ -2,3 +2,4 @@ vim.g.matchup_surround_enabled = true
 vim.g.matchup_delim_stopline = 1500
 vim.g.matchup_matchparen_deferred = true
 vim.g.matchup_matchparen_hi_surround_always = true
+vim.g.matchup_matchparen_offscreen = { method = "popup" }
