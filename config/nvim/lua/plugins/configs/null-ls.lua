002ef701ae964b070e9f8cd50d58f1d65f6c3484
Micke Lisinge <hi@micke.me>
2022-12-25 20:43:07 +0100

    Try null-ls

---
+++ config/nvim/lua/plugins/configs/null-ls.lua
@@ -0,0 +1,13 @@
+local null_ls = require("null-ls")
+
+null_ls.setup({
+    sources = {
+        -- null_ls.builtins.diagnostics.eslint,
+        -- null_ls.builtins.code_actions.eslint,
+        -- null_ls.builtins.formatting.eslint,
+        -- null_ls.builtins.completion.spell,
+        -- null_ls.builtins.diagnostics.rubocop,
+        -- null_ls.builtins.formatting.rubocop,
+        -- null_ls.builtins.code_actions.gitsigns,
+    },
+})
