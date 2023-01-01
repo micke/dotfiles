6967559bc1c03fa445e029323360b9e67ac4ebd7
Micke Lisinge <hi@micke.me>
2023-01-01 22:08:44 +0100

    Update augroup to use lua

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -43,13 +43,23 @@ function M.config()
     end
 
     if client.server_capabilities.documentHighlightProvider then
-      vim.api.nvim_exec([[
-      augroup lsp_document_highlight
-      autocmd! * <buffer>
-      autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
-      autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
-      augroup END
-        ]], false)
+      vim.api.nvim_create_augroup("lsp_document_highlight", {
+        clear = false
+      })
+      vim.api.nvim_clear_autocmds({
+        buffer = bufnr,
+        group = "lsp_document_highlight",
+      })
+      vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+        group = "lsp_document_highlight",
+        buffer = bufnr,
+        callback = vim.lsp.buf.document_highlight,
+      })
+      vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
+        group = "lsp_document_highlight",
+        buffer = bufnr,
+        callback = vim.lsp.buf.clear_references,
+      })
     end
   end
 
