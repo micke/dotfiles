dc67dc4543b1a1436847dc11d34fb4c595d6f9ea
Micke Lisinge <hi@micke.me>
2023-10-17 10:29:26 +0200

    Add copilot status to lualine

--- config/nvim/lua/plugins/lualine.lua
+++ config/nvim/lua/plugins/lualine.lua
@@ -3,6 +3,33 @@ local M = {
   event = "VeryLazy",
 }
 
+local Copilot = {
+  init = false,
+  status = "",
+}
+
+Copilot.setup = function ()
+  local api = require("copilot.api")
+  api.register_status_notification_handler(function(data)
+    if data.status == "Normal" then
+      status = ""
+    elseif data.status == "InProgress" then
+      status = "  "
+    else
+      status = data.status or "Offline"
+    end
+  end)
+end
+
+Copilot.get_status = function ()
+  if not Copilot.init then
+    Copilot.setup()
+    Copilot.init = true
+  end
+
+  return status
+end
+
 function M.config()
   require("lualine").setup {
     options = {
@@ -17,7 +44,7 @@ function M.config()
           path = 1
         }
       },
-      lualine_x = {'encoding', 'filetype'},
+      lualine_x = {Copilot.get_status, "encoding", "filetype"},
     },
   }
 end
